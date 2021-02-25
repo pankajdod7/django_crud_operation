06932d0e4b62c1f761444ba824a2aad2d7d5cfd1
Your Name <you@example.com>
2021-02-25 17:54:47 +0530

    orders crud

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -45,7 +45,8 @@ class Order(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=500, null=True, choices=STATUS)
     
-
+    def __str__(self):
+        return self.product.name
 
 
 
--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from accounts.views import home, products, customer
+from accounts.views import home, products, customer, createOrder, updateOrder, deleteOrder
 
 urlpatterns = [
     path("", home, name="home"),
     path("products/", products, name="products"),
     path("customers/<str:pk_test>/", customer, name="customer"),
+    path("create_order/", createOrder, name="create_order"),
+    path("update_order/<str:pk>/", updateOrder, name="update_order"),
+    path("delete_order/<str:pk>/", deleteOrder, name="delete_order"),
+
 ]
