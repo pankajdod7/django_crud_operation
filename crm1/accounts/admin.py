096dbefb04e1188949834bb902a643e2a070f8f7
Your Name <you@example.com>
2021-02-25 15:39:11 +0530

    Database models

--- crm1/accounts/admin.py
+++ crm1/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from accounts.models import Customer, Tag, Product, Order
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=['id', 'name', 'phone', 'email', 'date_created']
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display=['id', 'name']
+
+admin.site.register(Tag, TagAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['id', 'name', 'price', 'category', 'discription', 'date_created']
+
+admin.site.register(Product, ProductAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id', 'customer', 'product', 'date_created', 'status']
+
+admin.site.register(Order, OrderAdmin)
