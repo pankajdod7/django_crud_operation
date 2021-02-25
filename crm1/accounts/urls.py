51e4ecfff469c15068b62c85251fb971d3474b21
Your Name <you@example.com>
2021-02-25 12:25:37 +0530

    static files setup

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from accounts.views import home, products, customer
 
 urlpatterns = [
-    path("", home, name="dashboard"),
+    path("", home),
     path("products/", products, name="products"),
     path("customers/", customer, name="customer"),
 ]
